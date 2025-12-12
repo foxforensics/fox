0654d33e53cd68375565545cd4aea5c48d105221
Christian Uhsat <christian@uhsat.de>
2025-12-12 03:56:12 +0100

    feat: added basic format unit tests

---
+++ internal/pkg/data/format/journal/journal_test.go
@@ -0,0 +1,48 @@
+package journal
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/cuhsat/fox/v4/internal/pkg/data"
+)
+
+const file = "format/fox.journal"
+
+func BenchmarkDetect(b *testing.B) {
+	buf := data.Fixture(file)
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		_ = Detect(buf)
+	}
+}
+
+func BenchmarkConvert(b *testing.B) {
+	buf := data.Fixture(file)
+
+	b.ResetTimer()
+
+	for b.Loop() {
+		_, _ = Convert(buf)
+	}
+}
+
+func TestDetect(t *testing.T) {
+	if !Detect(data.Fixture(file)) {
+		t.Fatal("not detected")
+	}
+}
+
+func TestConvert(t *testing.T) {
+	buf, err := Convert(data.Fixture(file))
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if json.Valid(buf) {
+		t.Fatal("invalid json")
+	}
+}
