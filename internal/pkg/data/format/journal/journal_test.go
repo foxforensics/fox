b2f5f58efe7342e6a51e4bd11b53734040c935ab
Christian Uhsat <christian@uhsat.de>
2025-12-21 01:08:45 +0100

    feat: test files

--- internal/pkg/data/format/journal/journal_test.go
+++ internal/pkg/data/format/journal/journal_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/cuhsat/fox/v4/internal/pkg/data"
 )
 
-const file = "format/log.journal"
+const file = "format/test.journal"
 
 func BenchmarkDetect(b *testing.B) {
 	buf := data.Fixture(file)
