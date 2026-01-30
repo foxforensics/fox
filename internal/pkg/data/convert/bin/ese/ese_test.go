6366df32544087b18cded669add6f0eb1641eb52
Christian Uhsat <christian@uhsat.de>
2026-01-30 23:27:34 +0100

    test: added GOAT ntds.dit for testing

--- internal/pkg/data/convert/bin/ese/ese_test.go
+++ internal/pkg/data/convert/bin/ese/ese_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cuhsat/fox/v4/internal/pkg/data"
 )
 
-const file = "convert/test.ese"
+const file = "convert/test.ese.xz"
 
 func BenchmarkDetect(b *testing.B) {
 	buf := data.Fixture(file)
--- internal/pkg/data/test.go
+++ internal/pkg/data/test.go
@@ -2,10 +2,13 @@ package data
 
 import (
 	"bytes"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
 	"runtime"
+
+	"github.com/ulikunitz/xz"
 )
 
 const Sample = "fox.txt"
@@ -24,6 +27,24 @@ func Fixture(name string) []byte {
 	}
 
 	buf, err := os.ReadFile(filepath.Join(filepath.Dir(c), dir, name))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	if !HasMagic(buf, 0, []byte{
+		0xFD, 0x37, 0x7A, 0x58, 0x5A, 0x00,
+	}) {
+		return buf
+	}
+
+	r, err := xz.NewReader(bytes.NewReader(buf))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf, err = io.ReadAll(r)
 
 	if err != nil {
 		log.Fatalln(err)
