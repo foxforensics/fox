dbd3454407f9e2241b736fa947e0985cb7d77a69
Christian Uhsat <christian@uhsat.de>
2026-02-06 03:15:56 +0100

    feat: added emergency backup keys

--- internal/cmd/test/test.go
+++ internal/cmd/test/test.go
@@ -2,9 +2,13 @@ package test
 
 import (
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"strings"
+
+	"github.com/alecthomas/kong"
+	"github.com/xxtea/xxtea-go/xxtea"
 
 	cli "github.com/cuhsat/fox/v4/internal/cmd"
 
@@ -20,9 +24,9 @@ Tests suspicious files.
 fox test [FLAGS...] PATHS...
 
 Flags:
-  -D, --domain             File(s) contains domains
-  -U, --url                File(s) contains urls
-  -I, --ip                 File(s) contains ips
+  -D, --domain             file(s) contains domains
+  -U, --url                file(s) contains urls
+  -I, --ip                 file(s) contains ips
 
 Required:
   -k, --key=APIKEY         VirusTotal API key
@@ -31,12 +35,39 @@ Examples:
   $ fox test ioc.exe
 `)
 
+// Encrypted backup keys for emergency use
+const (
+	Key1 = "47ba3c085f105fff4fa186ce769f8a35f98bc3010fd8e25c9a90c1bf70696120b9fe1a5c6328bf0deae4eebdcc9f5df156a27efd923eaad648f3e8ab26fcc8f6753233b8"
+	Key2 = "44201ef4cbffe7edd1a7d2279a1fc3019700c3620da45d0542014b8a7be0fd7b53125c3e474c6db7360f4f538d56bfe15bd416b0d2a77c02a37d0ffc5015694b41c9f117"
+)
+
 type Test struct {
-	Key    string   `short:"k"`
-	Domain bool     `short:"D" xor:"domain,url,ip"`
-	Url    bool     `short:"U" xor:"domain,url,ip"`
-	Ip     bool     `short:"I" xor:"domain,url,ip"`
-	Paths  []string `arg:"" type:"path" optional:""`
+	Domain bool `short:"D" xor:"domain,url,ip"`
+	Url    bool `short:"U" xor:"domain,url,ip"`
+	Ip     bool `short:"I" xor:"domain,url,ip"`
+
+	// required
+	Key string `short:"k"`
+
+	// hidden
+	One string `xor:"one,two" hidden:""`
+	Two string `xor:"one,two" hidden:""`
+
+	// paths
+	Paths []string `arg:"" type:"path" optional:""`
+}
+
+func (cmd *Test) AfterApply(_ *kong.Kong, _ kong.Vars) error {
+	switch {
+	case len(cmd.One) > 0:
+		v, _ := hex.DecodeString(Key1)
+		cmd.Key = string(xxtea.Decrypt(v, []byte(cmd.One)))
+	case len(cmd.Two) > 0:
+		v, _ := hex.DecodeString(Key2)
+		cmd.Key = string(xxtea.Decrypt(v, []byte(cmd.Two)))
+	}
+
+	return nil
 }
 
 func (cmd *Test) Run(cli *cli.Globals) error {
