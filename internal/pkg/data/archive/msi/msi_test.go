87f8894793284a0e81279a4de05f58c691dcbf04
Christian Uhsat <christian@uhsat.de>
2026-02-25 18:28:37 +0100

    feat: added MSI unit test

---
+++ internal/pkg/data/archive/msi/msi_test.go
@@ -0,0 +1,48 @@
+package msi
+
+import (
+	"testing"
+
+	"github.com/cuhsat/fox/v4/internal/pkg/data"
+	"github.com/cuhsat/fox/v4/internal/pkg/test"
+)
+
+const file = "archive/fox.msi"
+
+func BenchmarkDetect(b *testing.B) {
+	buf := test.Fixture(file)
+
+	for b.Loop() {
+		_ = Detect(buf)
+	}
+}
+
+func BenchmarkExtract(b *testing.B) {
+	buf := test.Fixture(file)
+
+	for b.Loop() {
+		Extract(buf, "", "")
+	}
+}
+
+func TestDetect(t *testing.T) {
+	if !Detect(test.Fixture(file)) {
+		t.Fatal("not detected")
+	}
+}
+
+func TestExtract(t *testing.T) {
+	e := Extract(test.Fixture(file), "", "")
+
+	if len(e) != 1 {
+		t.Fatal("invalid entry count")
+	}
+
+	if e[0].Path != data.JoinPart("", test.Sample) {
+		t.Fatal("invalid entry path")
+	}
+
+	if !test.Assert(e[0].Data) {
+		t.Fatal("invalid entry data")
+	}
+}
