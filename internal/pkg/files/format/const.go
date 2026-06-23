6db827144532952febb3e83026bd3c0683e7146c
Christian Uhsat <christian@uhsat.de>
2026-06-24 00:03:41 +0200

    chore: refactored json formating

--- internal/pkg/files/format/const.go
+++ internal/pkg/files/format/const.go
@@ -9,16 +9,12 @@ import (
 const Prefix = ""
 const Indent = "  "
 
-func Error(err error) string {
-	return fmt.Sprintf(`{"error": "%s"}`, err.Error())
-}
-
 func AsJSON(a any) string {
 	b, err := json.MarshalIndent(a, Prefix, Indent)
 
 	if err != nil {
 		slog.Error(err.Error())
-		return Error(err)
+		return JsonError(err)
 	}
 
 	return string(b)
@@ -29,8 +25,12 @@ func AsJSONL(a any) string {
 
 	if err != nil {
 		slog.Error(err.Error())
-		return Error(err)
+		return JsonError(err)
 	}
 
 	return string(b)
+}
+
+func JsonError(err error) string {
+	return fmt.Sprintf(`{"error": "%s"}`, err.Error())
 }
