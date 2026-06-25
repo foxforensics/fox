62927dc30eb408b00676b43bea9bfdb6b08c69c6
Christian Uhsat <christian@uhsat.de>
2026-06-25 17:31:37 +0200

    fix: unique complexity

--- internal/pkg/types/types.go
+++ internal/pkg/types/types.go
@@ -1,33 +1,3 @@
 package types
 
-import "sync"
-
 type Nil struct{}
-
-type Set struct {
-	sync.RWMutex
-	m map[any]Nil
-}
-
-func NewSet() *Set {
-	return &Set{m: make(map[any]Nil)}
-}
-
-func (set *Set) Len() int {
-	set.RLock()
-	defer set.RUnlock()
-	return len((*set).m)
-}
-
-func (set *Set) Set(key any) {
-	set.Lock()
-	(*set).m[key] = Nil{}
-	set.Unlock()
-}
-
-func (set *Set) Has(key any) bool {
-	set.RLock()
-	_, ok := (*set).m[key]
-	set.RUnlock()
-	return ok
-}
