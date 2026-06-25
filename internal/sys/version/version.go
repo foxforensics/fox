09490098e148aa7970da5017f2ad20c5fae16790
Christian Uhsat <christian@uhsat.de>
2026-06-25 17:30:24 +0200

    fix: dev version

--- internal/sys/version/version.go
+++ internal/sys/version/version.go
@@ -1,10 +1,16 @@
 // Package version will be set upon release.
 package version
 
-var Number = "dev"
-var Commit = "none"
+var Number = "0.0.0"
+var Commit = "dev"
 
 // ID returns to shortened commit id.
+//
+//goland:noinspection GoBoolExpressions
 func ID() string {
-	return Commit[:min(len(Commit), 6)]
+	if len(Commit) > 0 {
+		return Commit[:min(len(Commit), 6)]
+	}
+
+	return "none"
 }
